2540c75dd3568c83995f43d474fe7e10abaf0f1a
pavitra0 <143537026+pavitra0@users.noreply.github.com>
2023-11-15 20:44:32 +0530

    Add files via upload
    animation kind of responsive project is uploaded now

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-let istatus = document.querySelector("h5")
+// Shery.imageEffect("#back" , {style: 2, debug: true})
 
-let btn = document.querySelector("#add")
+let elms = document.querySelectorAll('.elm')
+elms.forEach((elm)=>{
+    let h1s = elm.querySelectorAll('h1');
+    let index = 0;
+   var animating = false
 
-// let remove = document.querySelector("#remove")
-let isopen = 0
-btn.addEventListener("click", ()=>{
-    if(isopen ==0){
-    istatus.innerHTML = "Blue Amplification"
-    btn.innerHTML="Remove"
-    isopen = 1
-    }else{
-        istatus.innerHTML = "Seal"
-        isopen = 0
-        btn.innerHTML="Add Curse"
-    }
+    document.querySelector('#main').addEventListener('click', ()=>{
+  if(!animating){
+//    animating = true;
+    gsap.to(h1s[index], {
+        top:'-=100%',
+        ease: Expo.easeInOut,
+        duration:1,
+        onComplete: ()=>{
+            gsap.set(this._targets[0], {top: '100%'})
+            
+        },
+        
+    });
+
+   index === h1s.length - 1 ? (index = 0) : index++;
+
+    gsap.to(h1s[index], {
+        top:'-=100%',
+        ease: Expo.easeInOut,
+        duration:1,
+    });
+  }
 })
-// remove.addEventListener("click",()=>{
-//     istatus.innerHTML =  "Strong"
-// })
+
+})
+animating = false;
